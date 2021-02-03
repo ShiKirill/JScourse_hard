dab37c6b6783b48588f50adc7eb1defa100d914b
ShiKirill <shiraevkirill@gmail.com>
2021-02-04 01:10:47 +0700

    Change array

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 "use strict";
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
 function isPrime(num){
   for (let i = 2; i < +num; i++){
     if ((+num % i) === 0){
@@ -15,11 +9,7 @@ function isPrime(num){
   return true;
 }
 
-let arr = [];
-
-for (let i = 0; i< 7; i++){
-  arr[i] = String(getRandomInt(1500, 20000));
-}
+let arr = ['24512', '12313', '431231', '51231', '25151', '951819', '125123'];
 
 let tmp = 0;
 for (let i=0; i<7; i++){
