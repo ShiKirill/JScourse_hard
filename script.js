f86c906064bab898913c5d10f437d34ca81ba2cf
ShiKirill <shiraevkirill@gmail.com>
2021-02-04 16:14:57 +0700

    Lesson 6_hard

--- script.js
+++ script.js
@@ -1,48 +1,64 @@
 "use strict";
 
-function isNumber(n){
-  return (!isNaN(parseFloat(n)) && isFinite(n));
+function isNumber(n) {
+    return (!isNaN(parseFloat(n)) && isFinite(n));
 }
 
 function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function game(){
-  let botNumber =  getRandomInt(1, 100);
+function game() {
+    let botNumber = getRandomInt(1, 100);
+    let userAttempts = 10;
     function gameProgress() {
-        let userNumber = prompt('Угадай число от 1 до 100');
-        if (isNumber(userNumber)){
-          if (+userNumber>botNumber) {
-            let check = confirm ('Загаданное число меньше. Хотите продолжить?');
-            if (check) {
-              gameProgress();
+        if (userAttempts > 0) {
+            let userNumber = prompt('Угадай число от 1 до 100');
+            if (isNumber(userNumber)) {
+                if (+ userNumber > botNumber) {
+                    userAttempts--;
+                    let check = confirm(`Загаданное число меньше, осталось ${userAttempts} попыток. Хотите продолжить?`);
+                    if (check) {
+                        gameProgress();
+                    } else {
+                        return alert('Игра окончена');
+                    }
+                } else if (+ userNumber < botNumber) {
+                    userAttempts--;
+                    let check = confirm(`Загаданное число больше, осталось ${userAttempts} попыток. Хотите продолжить?`);
+                    if (check) {
+                        gameProgress();
+                    } else {
+                        return alert('Игра окончена');
+                    }
+                } else {
+                    let check = confirm('Поздравляю, вы угадали!!! Хотели бы сыграть ещё?');
+                    if (check) {
+                        game();
+                    } else {
+                        return alert('Игра окончена');
+                    }
+                }
             } else {
-              return alert('Игра окончена');
+                let check = confirm('Введи число!');
+                if (check) {
+                    gameProgress();
+                } else {
+                    return alert('Игра окончена');
+                }
             }
-          } else if (+userNumber<botNumber) {
-            let check = confirm ('Загаданное число больше. Хотите продолжить?');
-            if (check) {
-              gameProgress();
-            } else {
-              return alert('Игра окончена');
-            }
-          } else {
-            return alert('Поздравляю, вы угадали!!!');
-          }
-      } else {
-        let check = confirm('Введи число!');
-        if (check) {
-          gameProgress();
         } else {
-          return alert('Игра окончена');
+            let check = confirm('Попытки закончились. Хотите сыграть ещё?');
+            if (check) {
+                game();
+            } else {
+                return alert('Игра окончена');
+            }
         }
-      }
     }
-
-  gameProgress();
+    gameProgress();
 }
 
 game();
