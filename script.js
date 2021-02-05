0bb41b87b860afc4b4b7a5247401df841de33f98
ShiKirill <shiraevkirill@gmail.com>
2021-02-06 02:11:10 +0700

    Lesson 7 modified

--- script.js
+++ script.js
@@ -9,7 +9,11 @@ if (today <0) {today = 6;
 for (let i=0 ; i < week.length ; i++){
     let div = document.createElement('div');
     if (today === i) {
-        div.innerHTML = `<b>${week[i]}</b>`;
+        if (today < week.length-2){
+            div.innerHTML = `<b>${week[i]}</b>`;
+        } else {
+            div.innerHTML = `<b><i>${week[i]}</i></b>`;
+        }
         document.body.append(div);
     } else if (i < week.length-2){
         div.innerHTML = `${week[i]}`;
