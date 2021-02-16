b247b3863cef8d9c23838a0797edc8599c2d133b
ShiKirill <shiraevkirill@gmail.com>
2021-02-17 03:40:29 +0700

    Lesson 14, part 3

--- script.js
+++ script.js
@@ -11,20 +11,44 @@ function DomElement(selector, height, width, bg, fontSize){
             const div = document.createElement('div');
             div.classList.add(selector.slice(1));
             div.style.cssText = 'height:' + this.height + ';width:' + this.width + ';background-color:' + this.bg + ';font-size:'+ this.fontSize+';';
-            div.textContent = 'Hello world!';
+            
             document.body.append(div);
         } else if (selector[0] === '#') {
             const p = document.createElement('p');
             p.id = selector.slice(1);
             p.style.cssText = 'height:' + this.height + ';width:' + this.width + ';background-color:' + this.bg + ';font-size:'+ this.fontSize+';';
-            p.textContent = 'Hello world!';
+            
             document.body.append(p);
         } else {
             console.log('Ошибка в selector!');
         }
     };
 }
+DomElement.prototype.moveObject = function(){
+    const object = document.querySelector(this.selector);
+    object.style.cssText += 'position: absolute;';
+    let right = 0;
+    let bottom = 0;
+    document.addEventListener('keydown', function(event) {
+    if (event.key ==='ArrowLeft') {
+        right -=10;
+        object.style.left = right + 'px';
+    } else if (event.key ==='ArrowRight') {
+        right +=10;
+        object.style.left = right + 'px';
+    } else if (event.key ==='ArrowUp') {
+        bottom -= 10;
+        object.style.top = bottom + 'px';
+    } else if (event.key ==='ArrowDown') {
+        bottom += 10;
+        object.style.top = bottom + 'px';
+    }
+    });
+};
 
-let newObject = new DomElement('.block', '200px', '500px', '#aa2812','24px');
+let newObject = new DomElement('.block', '100px', '100px', 'red');
 
-newObject.createElem();
+document.addEventListener("DOMContentLoaded", function() {
+    newObject.createElem();
+    newObject.moveObject();
+});
